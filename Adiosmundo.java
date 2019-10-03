9b1bc4822001f71a27a91438f616bdf0aa973bf3
Patxi Sanchis <patxisldaw19@gmail.com>
2019-10-03 19:32:24 +0200

    Mejorando el adiosmundo

--- Adiosmundo.java
+++ Adiosmundo.java
@@ -4,13 +4,11 @@ package prueba;
 public class Adiosmundo {
 
 	public static void main(String[] args) {
-String amarillo = "\033[33m";
-String verde = "\033[32m";
-
-
-
+		String amarillo = "\033[33m";
+		String verde = "\033[32m";
                 System.out.println(amarillo + "Adios mundo");
-        System.out.println(verde + "Pero... volvere");
+        	System.out.println(verde + "Pero... volvere");
+		System.out.println("Modificado por Patxi");
         }
 
 }
